0b25a8de21d2a0f7b2905a1a2383f98497b3ad38
Souissi oumaima <99618076+FuturDev22@users.noreply.github.com>
2023-10-17 10:36:39 +0200

    Initial commit for Spring Boot project

---
+++ src/main/java/com/example/projetdev001/Projetdev001Application.java
@@ -0,0 +1,13 @@
+package com.example.projetdev001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetdev001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetdev001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetdev001/Projetdev001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetdev001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetdev001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
